69ef299620f2b0500ae00f21ce570ccf2d09d7c5
chenxueli <zjphchenxueli@163.com>
2022-10-27 13:42:23 +0800

    去除无用的console

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,3 @@
-<script setup lang="ts">
-import { getRequestConfig } from '@dc-basic-component/config'
-
-console.log(getRequestConfig())
-</script>
-
 <template>
   <main font-sans p="x-4 y-20px" h-100vh text="gray-700 dark:gray-200" overflow-hidden>
     <router-view />
